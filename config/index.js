4297baf744c652270244a769e2ee8ccb04e3d78b
porteFinder <smurfhalt@gmail.com>
2020-02-23 12:31:49 +0100

    clean env.....

--- config/index.js
+++ config/index.js
@@ -1,12 +1,12 @@
 import dotenv from "dotenv";
 
-process.env.NODE_ENV = process.env.NODE_ENV || "development";
-
 const envFound = dotenv.config();
 
 if (!envFound) {
   throw new Error("Can't find .env file");
 }
+
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
 export default {
   port: parseInt(process.env.PORT, 10),
@@ -17,7 +17,7 @@ export default {
     level: process.env.LOG_LEVEL || "silly"
   },
 
-  jwtSecret: "s0m3_uLtr4-ran2om_3tr1ng",
+  jwtSecret: process.env.jwtSecret,
 
   api: {
     prefix: "/api"
